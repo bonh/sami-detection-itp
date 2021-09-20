a57f01e8b97cc13ba3bc42235ee3e3fba4166dad
Henning Bonart <bonart@nmf.tu-darmstadt.de>
2021-09-20 19:52:31 +0200

    added methods to create artifical itp signal

---
+++ src/uq_itp/utilities.py
@@ -0,0 +1,55 @@
+import numpy as np
+from scipy.special import erf
+
+def skewNormal(x, t, v=1.0, sigma=1.0, alpha=0.0):
+    '''
+    Skew normal distribution.
+    
+    INPUT:
+        x: position
+        t: time
+        v: velocity
+        sigma: width parameter
+        alpha: skewness parameter (simple Gaussian for alpha=0.0)
+        
+    RETURN:
+        skew normal
+    '''
+    return 1/np.sqrt(2*np.pi)/sigma * np.exp(-(x-v*t)**2/2/sigma**2) * (1 + erf(alpha*(x-v*t)/np.sqrt(2)/sigma))
+
+def artificalsignal(S0=1.0, sigma=1.0, v=1.0, alpha=1.0, noise='none', nstrength=1.0, width=512, height=54, nframes=500, **kwargs):
+    '''
+    Creates an ITP signal.
+    
+    INPUT:
+        S0: total intensity
+        sigma: signal width
+        v: signal velocity
+        alpha: skewness (for alpha=0, the signal has Gaussian shape)
+        noise: type of background noise ('none', 'uniform', 'log-normal')
+        nstrength: noise strength
+        width: frame width in px
+        height: frame height in px
+        nframes: number of frames
+    
+    RETURN:
+        data: numpy array of image frames 
+    '''
+    # data array
+    data = np.zeros((height, width, nframes))
+    
+    # noise array
+    ndata = np.zeros((height, width, nframes))
+    ln_mean = kwargs.get('ln_mean', 0.5)
+    ln_sigma = kwargs.get('ln_sigma', 0.05)
+    if noise == 'uniform':
+        ndata = np.random.random_sample((height,width,nframes))
+    elif noise == 'log-normal':
+        ndata = np.random.lognormal(mean=ln_mean, sigma=ln_sigma, size=(height,width,nframes))
+        
+    # create signal
+    x = np.linspace(0, width, width, endpoint=False)
+    for i in range(nframes):
+        data[:,:,i] = np.array([skewNormal(x, i, v=v, sigma=sigma, alpha=alpha)]*height)
+
+    return data*S0 + ndata*nstrength
