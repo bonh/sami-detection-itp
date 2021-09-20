3bd61d2c369288fb93fdf493d65ecaa36c0c412f
Henning Bonart <bonart@nmf.tu-darmstadt.de>
2021-09-20 11:39:10 +0200

    adapted input method

---
+++ src/uq_itp/averaging.py
@@ -0,0 +1,28 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.12.0
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %load_ext autoreload
+# %autoreload 2
+
+import matplotlib.pyplot as plt
+import input
+
+inname = "/home/cb51neqa/Downloads/01.nd2"
+
+data = input.load_nd_data(inname)
+
+plt.imshow(data[:,:,100]);
+
+
--- src/uq_itp/input.py
+++ src/uq_itp/input.py
@@ -2,14 +2,17 @@ import numpy as np
 from nd2reader import ND2Reader
 
 def load_nd_data(inname, startframe=0, endframe=-1):
-    with ND2Reader(inname) as rawimages:        
+    with ND2Reader(inname) as rawimages:
+        print(rawimages)
         rawimages.bundle_axes = 'yx' # defines which axes will be present in a single frame
-        rawimages.iter_axes = 'z' # defines which axes will be the index axis; z-axis is the time axis
+        rawimages.iter_axes = 't' # defines which axes will be the index axis; t-axis is the time axis
     
         # determine metadata of the images
         height = rawimages.metadata["height"]
         width = rawimages.metadata["width"]
         nframes = rawimages.metadata["num_frames"]
+        
+        print("\nheight = {}, width = {}, nframes = {}".format(height, width, nframes))
         
         if endframe == -1:
             end = len(rawimages)
@@ -30,4 +33,5 @@ def load_nd_data(inname, startframe=0, endframe=-1):
         for frame in np.arange(startframe, end, 1):
             data[:,:,frame] = rawimages[frame]
     
+        print("\ndata shape = {}".format(data.shape))
         return data
