132beb553aa042ca03b7956ca0aabca8e401bc64
Henning Bonart <bonart@nmf.tu-darmstadt.de>
2021-09-20 11:05:48 +0200

    added method to read nd2 data files and transform into numpy array

---
+++ src/uq_itp/input.py
@@ -0,0 +1,33 @@
+import numpy as np
+from nd2reader import ND2Reader
+
+def load_nd_data(inname, startframe=0, endframe=-1):
+    with ND2Reader(inname) as rawimages:        
+        rawimages.bundle_axes = 'yx' # defines which axes will be present in a single frame
+        rawimages.iter_axes = 'z' # defines which axes will be the index axis; z-axis is the time axis
+    
+        # determine metadata of the images
+        height = rawimages.metadata["height"]
+        width = rawimages.metadata["width"]
+        nframes = rawimages.metadata["num_frames"]
+        
+        if endframe == -1:
+            end = len(rawimages)
+        else:
+            if endframe < nframes:
+                end = endframe
+            else:
+                end = nframes
+                print("endframe < len(rawimages)")
+    
+        # Y x X x N
+        data = np.zeros((height, width, end))
+    
+        #data = rawimages
+        #print(np.mean(data)
+    
+        # load image data into data array
+        for frame in np.arange(startframe, end, 1):
+            data[:,:,frame] = rawimages[frame]
+    
+        return data
