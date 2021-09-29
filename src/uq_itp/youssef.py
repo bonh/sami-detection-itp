7f21c8f4da0cbe49122eaf20b33f467c8e31313c
Henning Bonart <bonart@nmf.tu-darmstadt.de>
2021-09-29 14:12:54 +0200

    added questions

--- src/uq_itp/youssef.py
+++ src/uq_itp/youssef.py
@@ -14,6 +14,11 @@
 #     name: python3
 # ---
 
+# Questions: 
+# * We often record images in which the sample has not reached the measurement window yet. Basically, we record only noise. The more images with pure noise (so without sample) we include in our algorithm the worse the result. How can we decide if we should include a specific image or not? This question corresponds to the `startframe` and `endframe` variables below.
+# * We find the best velocity by minimization the high density interval of a Bayesian fit function. How can we include the velocity as an uncertain parameter into the Bayesian inference problem? The data somehow depends on the velocity?
+#
+
 # + tags=[]
 import numpy as np
 import matplotlib.pyplot as plt
@@ -33,7 +38,7 @@ plt.rcParams['figure.figsize'] = [10, 5]
 # 3. Substract a mean background image
 
 # + tags=[]
-inname = "/home/cb51neqa/projects/itp/exp_data/ITP_AF647_5µA/AF_10ng_l/001.nd2"
+inname = "/home/cb51neqa/projects/itp/exp_data/ITP_AF647_5µA/AF_0.1ng_l/001.nd2"
 
 channel_lower = 27
 channel_upper = 27
@@ -90,7 +95,7 @@ def shift_data(data, v, fps, px):
 
 
 # +
-v = 0.4e-3
+v = 0.4e-3 # pick a random velocity here
 fps = 46
 px = 1.6e-6
 
@@ -108,10 +113,63 @@ axs[0].legend()
 axs[1].imshow(data_shifted.T, aspect="auto",origin="lower")
 axs[1].set_xlabel("Length")
 axs[1].set_ylabel("Time");
-axs[1].text(130,250, "Samples shifted with v={}mm/s, so it's not perfect.".format(v*1000), color="red",fontsize=14);
+axs[1].text(130,250, "Samples shifted with v={}mm/s, so it's far from perfect.".format(v*1000), color="red",fontsize=14);
+
+# How can we find a better velocity value?
+
+# +
+step = 20
+
+corr = np.zeros(data.shape)
+for i in range(0,data.shape[1]-step):
+    corr[:,i] = np.correlate(data[:,i], data[:,i+step], "same")
+    
+corr[int(512/2),:] = 0 # remove auto-correlation
+scaler = preprocessing.StandardScaler().fit(corr)
+corr = scaler.transform(corr)
+# -
+
+time = 100
+x = np.linspace(-corr.shape[0]/2, corr.shape[0]/2, corr.shape[0])
+fig,axs = plt.subplots(2,1,sharex=True)
+#axs[0].plot(x, corr[:,time], label="correlated")
+axs[0].plot(x, np.mean(corr, axis=1), label="correlated/averaged")
+axs[0].plot(x, np.mean(corr[:,startframe:endframe], axis=1), label="correlated/cut/averaged")
+axs[0].set_ylabel("corr");
+axs[0].legend()
+axs[1].imshow(corr.T, aspect="auto",origin="lower", extent=[x[0],x[-1],0,corr.shape[1]])
+axs[1].set_xlabel("Length")
+axs[1].set_ylabel("Time");
+
+# +
+tmp = np.mean(corr[:,startframe:endframe], axis=1)
+
+d = 512/2-np.argmax(tmp)
+dx = d*px
+t = step
+dt = t/fps
+
+v = dx/dt
+print(v*1e4)
+# -
+
+data_shifted = shift_data(data, v, fps, px)
+
+time = 100
+fig,axs = plt.subplots(2,1,sharex=True)
+axs[0].plot(data[:,time], label="raw")
+axs[0].plot(data_shifted[:,time], label="shifted")
+axs[0].plot(np.mean(data_shifted, axis=1), label="shifted/averaged")
+axs[0].plot(np.mean(data_shifted[:,startframe:endframe], axis=1), label="shifted/cut/averaged")
+axs[0].set_ylabel("Intensity");
+axs[0].legend()
+axs[1].imshow(data_shifted.T, aspect="auto",origin="lower")
+axs[1].set_xlabel("Length")
+axs[1].set_ylabel("Time");
+axs[1].text(130,250, "Samples shifted with v={}mm/s, way better.".format(v*1000), color="red",fontsize=14);
 
 
-# 6. Create Bayesian model of the sample distribution at a specific time (almost Gaussian). Find the velocity of the sample by optimization. In a way, we treat the velocity as a hyperparameter.
+# 6. Create Bayesian model of the sample distribution at a specific time (almost Gaussian). Find the velocity of the sample by optimization. In a way, we treat the velocity as a hyperparameter (?).
 
 # +
 def model_signal(amp, cent, sig, baseline, x):
@@ -121,10 +179,10 @@ def create_model(data, x):
     model = pm.Model()
     with model:
         # prior peak
-        amp = pm.HalfNormal('amplitude', 0.5)
-        cent = pm.Normal('centroid', 250, 200)
+        amp = pm.HalfNormal('amplitude', 1) 
+        cent = pm.Normal('centroid', 250, 200) 
         base = pm.Normal('baseline', 0, 0.5)
-        sig = pm.HalfNormal('sigma', 50)
+        sig = pm.HalfNormal('sigma', 50)  
 
         # forward model signal
         signal = pm.Deterministic('signal', model_signal(amp, cent, sig, base, x))
@@ -149,13 +207,11 @@ def functional(v):
     with create_model(avg, x) as model:
         trace = pm.sample(10000, return_inferencedata=False, cores=4, progressbar=False)
         
-    alpha = 1e-5
     hdi_centroid = az.hdi(trace["centroid"])
     hdi_sigma = az.hdi(trace["sigma"])
     result = 1/2*np.sqrt(
         (hdi_centroid[1] - hdi_centroid[0])**2 \
-        + (hdi_sigma[1] - hdi_sigma[0])**2) \
-        + alpha/2*np.sqrt(v**2)
+        + (hdi_sigma[1] - hdi_sigma[0])**2)
     
     return result
 
@@ -166,7 +222,8 @@ from gradient_free_optimizers import *
 def functional2(para):
     return -functional(para["v"])
 
-search_space = {"v": np.arange(2e-4, 4e-4, 1e-6)}
+# take the v calculated before
+search_space = {"v": np.arange(v-1e-4/2, v+1e-4/2, 1e-6)}
 
 #opt = RandomSearchOptimizer(search_space)
 #opt = BayesianOptimizer(search_space)
@@ -175,7 +232,7 @@ opt = EvolutionStrategyOptimizer(search_space)
 opt.search(functional2, n_iter=20, early_stopping={"n_iter_no_change":3}, max_score=-0.1)
 # -
 
-v = 2.39e-4#opt.best_para["v"]
+v = opt.best_para["v"]
 data_shifted = shift_data(data, v, fps, px) 
 avg = np.mean(data_shifted[:,startframe:endframe], axis=1)
 
@@ -184,7 +241,7 @@ fig,axs = plt.subplots(2,1,sharex=True)
 axs[0].plot(data[:,time], label="raw")
 axs[0].plot(data_shifted[:,time], label="shifted")
 axs[0].plot(np.mean(data_shifted, axis=1), label="shifted/averaged")
-axs[0].plot(np.mean(data_shifted[:,startframe:endframe], axis=1), label="shifted/cut/averaged")
+axs[0].plot(avg, label="shifted/cut/averaged")
 axs[0].set_ylabel("Intensity");
 axs[0].legend()
 axs[1].imshow(data_shifted.T, aspect="auto",origin="lower")
@@ -205,10 +262,8 @@ summary
 az.plot_posterior(trace, var_names=["amplitude", "centroid", "sigma", "baseline", "sigma_noise"]);
 
 plt.plot(avg, label="shifted/cut/averaged")
-plt.plot(data[:,time], label="raw")
+#plt.plot(data[:,time], label="raw")
 map_estimate = summary.loc[:, "mean"]
 plt.plot(
     model_signal(map_estimate["amplitude"], map_estimate["centroid"], map_estimate["sigma"], map_estimate["baseline"], x), label="model");
 plt.legend();
-
-
