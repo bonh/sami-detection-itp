b968517cabda8104915739b8219f7a94325b397a
Henning Bonart <bonart@nmf.tu-darmstadt.de>
2021-10-19 16:08:12 +0200

    rewrote example so that the final detection is made on the raw data not the crosscorrelated functions

--- src/uq_itp/example.py
+++ src/uq_itp/example.py
@@ -29,6 +29,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import dataprep
+import helper
 import utilities
 from matplotlib.patches import Rectangle
 import arviz as az
@@ -42,7 +43,7 @@ plt.rcParams['animation.html'] = "jshtml"#'html5'
 plt.rcParams['figure.dpi'] = 72
 
 # +
-inname = "/home/cb51neqa/projects/itp/exp_data/ITP_AF647_5µA/AF_10ng_l/001.nd2"
+inname = "/home/cb51neqa/projects/itp/exp_data/ITP_AF647_5µA/AF_0.1ng_l/003.nd2"
 
 # to cut images to channel
 channel_lower = 27
@@ -60,12 +61,8 @@ px = 1.6e-6 # size of pixel (m/px)
 # At first, we perform raw data processing (load microscopy images, cut height of images to microchannel, substract mean background image). This results in a 4D data array (height, length, frame, intensity). This corresponds to step 1 in the flowsheet.
 
 # + tags=[]
-data_raw = dataprep.load_nd_data(inname, verbose=False)
-data_raw = dataprep.cuttochannel(data_raw, channel_lower, channel_upper)
-background = np.mean(data_raw[:,:,0:10],axis=2)
-data_raw = dataprep.substractbackground(data_raw, background)
+data_raw = helper.raw2images(inname, (channel_lower, channel_upper))
 
-# + tags=[]
 height = data_raw.shape[0]
 length = data_raw.shape[1]
 nframes = data_raw.shape[2]
@@ -97,9 +94,8 @@ tmp = dataprep.averageoverheight(data_raw)
 scaler = preprocessing.StandardScaler().fit(tmp)
 data = scaler.transform(tmp)
 
+time = 200
 x = np.linspace(0, length*px*1e3, length)
-
-time = 100
 fig,axs = plt.subplots(2,1,sharex=True, figsize=(10,5))
 axs[0].plot(x, data[:,time])
 axs[0].set_title("frame = {}".format(time))
@@ -121,13 +117,13 @@ axs[0].plot(corr[:,time])
 axs[0].set_title("frame = {}".format(time))
 axs[0].set_ylabel("intensity (AU)");
 axs[1].imshow(corr.T, aspect="auto", origin="lower")
-axs[1].set_xlabel("lag ???")
-axs[1].set_ylabel("???");
+axs[1].set_xlabel("lag (px)")
+axs[1].set_ylabel("intensity (AU)");
 
-#  As the velocity of the sample is pratically constant, the correlation between the frames should be almost identical (except for the noise). Then we average all the cross correlation functions to reduce the noise (step 4). 
+#  As the velocity of the sample is pratically constant, the correlation between the frames should be almost identical (except for the noise). So we can average all the cross correlation functions to reduce the noise (step 4). 
 
 # +
-corr_mean = np.mean(corr[:,50:200], axis=1).reshape(-1, 1)
+corr_mean = np.mean(corr[:,startframe:endframe], axis=1).reshape(-1, 1)
 x = np.linspace(-corr_mean.shape[0]/2, corr_mean.shape[0]/2, corr_mean.shape[0])
 
 # clean the correlation data
@@ -135,8 +131,8 @@ x = np.linspace(-corr_mean.shape[0]/2, corr_mean.shape[0]/2, corr_mean.shape[0])
 corr_mean[int(corr_mean.shape[0]/2)] = 0
 #cut everything right of the middle (because we know that the velocity is positiv)
 corr_mean = corr_mean[0:int(corr_mean.shape[0]/2)]
+
 x = x[0:int(corr_mean.shape[0])]
-# -
 
 corr_singleframe = corr[0:int(corr.shape[0]/2),time]
 
@@ -146,16 +142,11 @@ corr_mean = scaler.transform(corr_mean).flatten()
 
 scaler = preprocessing.MinMaxScaler().fit(corr_singleframe.reshape(-1,1))
 corr_singleframe = scaler.transform(corr_singleframe.reshape(-1,1)).flatten()
+
+
 # -
 
-plt.plot(x, corr_singleframe, label="single frame")
-plt.plot(x, corr_mean, label="averaged")
-plt.xlabel("lag (pixel)")
-plt.ylabel("intensity (AU)")
-plt.legend()
-
-
-# If we compare the blue signal with the orange signal we observe that the noise is greatly reduced. Let's quantify the improvement by fitting a Gaussian function to the single frame signal and the averaged signal (we know from physics that the sample distribution in the channel should be similar to a Gaussian so the correlation should follow a Gaussian tooplt.plot(corr_singleframe)).
+# To find the velocity of the sample we now fit a Gaussian function to the averaged cross correlation functions (we know from physics that the sample distribution in the channel should be similar to a Gaussian so the correlation should follow a Gaussian too).
 
 # +
 def model_signal(amp, cent, sig, baseline, x):
@@ -167,7 +158,7 @@ def create_signalmodel(data, x):
         amp = pm.HalfNormal('amplitude', 1)
         cent = pm.Normal('centroid', 125, 50)
         base = pm.Normal('baseline', 0, 0.5)
-        sig = pm.HalfNormal('sigma', 10)
+        sig = pm.HalfNormal('sigma', 20)
 
         # forward model signal
         signal = pm.Deterministic('signal', model_signal(amp, cent, sig, base, x))
@@ -178,65 +169,57 @@ def create_signalmodel(data, x):
         # likelihood
         likelihood = pm.Normal('y', mu = signal, sd=sigma_noise, observed = data)
         
+        # calculate the velocity and the signal-to-noise distributions from the uncertain parameters of the Gaussian
+        velocity = pm.Deterministic("velocity", cent*px/(lagstep/fps)*1e6)
+        snr = pm.Deterministic("snr", amp/sigma_noise)
+        
         return model
 
 
-# +
-x *= -1
-with create_signalmodel(corr_mean, x) as model:
-    trace_mean = pm.sample(20000, return_inferencedata=True, cores=4)
-
-with create_signalmodel(corr_singleframe, x) as model:
-    trace_singleframe = pm.sample(20000, return_inferencedata=True, cores=4)
 # -
 
-summary_mean = az.summary(trace_mean, var_names=["amplitude", "centroid", "sigma", "baseline", "sigma_noise"])
-summary_singleframe = az.summary(trace_singleframe, var_names=["amplitude", "centroid", "sigma", "baseline", "sigma_noise"])
+x *= -1
+models = []
+with create_signalmodel(corr_mean, x) as model:
+    trace_mean = pm.sample(10000, return_inferencedata=True, cores=4)
+    models.append(model)
 
+ppc = pm.sample_posterior_predictive(trace_mean, model=models[0])
+idata = az.from_pymc3(
+        posterior_predictive=ppc,
+        model=models[0]
+    )
+
+# Now we can display the averaged cross correlation functions together with the Gaussian fit and the .95 credible interval.
+
+# + tags=[]
+hdi = az.hdi(idata.posterior_predictive, hdi_prob=.95)
 fig,axs = plt.subplots(1,1,sharex=True, figsize=(10,5))
-axs.plot(x, corr_mean, label="averaged", alpha=0.8)
-axs.plot(x, corr_singleframe, label="single", alpha=0.8)
-map_estimate = summary_singleframe.loc[:, "mean"]
-axs.plot(x, 
-    model_signal(map_estimate["amplitude"], map_estimate["centroid"], map_estimate["sigma"], map_estimate["baseline"], x), label="fit, single", alpha=0.5)
-map_estimate = summary_mean.loc[:, "mean"]
-axs.plot(x, 
-    model_signal(map_estimate["amplitude"], map_estimate["centroid"], map_estimate["sigma"], map_estimate["baseline"], x), label="fit, mean", alpha=0.5)
+axs.plot(x, corr_mean, label="averaged", alpha=0.8, color="black")
+axs.plot(x, idata.posterior_predictive.mean(("chain", "draw"))["y"], color="red", label="fit")
+axs.fill_between(x, hdi["y"][:,0], hdi["y"][:,1], alpha=0.2, color="blue", label=".95 HDI")
 axs.legend();
 axs.set_xlabel("lag (px)")
-axs.set_ylabel("intensity (AU)")
-
-# +
-map_estimate = summary_singleframe.loc[:, "mean"]
-snr_before = map_estimate["amplitude"]/map_estimate["sigma_noise"]
-map_estimate = summary_mean.loc[:, "mean"]
-snr_after = map_estimate["amplitude"]/map_estimate["sigma_noise"]
-
-print("snr_before = {}\nsnr_after = {}".format(snr_before, snr_after))
-print("improvement = snr_after/snr_before = {}".format(snr_after/snr_before))
+axs.set_ylabel("intensity (AU)");
 # -
 
-# As a result we have a reduced the noise and achieved a great improvement of the signal-to-noise ratio.
-#
-# Let's calculate the velocity of the sample from the fit of the averaged cross corelation functions.
+# Futhermore, let's have a look at the velocity and the signal-to-noise ratio calculated from the posterior distribution. The HDI interval of the velocity is narrow and centered around a physical reasonable value, and the snr is always greater than one. Taking both factors into mind we conclude that there might be a sample inside the channel and the estimated mean velocity is an accurate value (you can do the same analysis using a no-sample image). Therefore we continue the analysis.
 
-# +
-map_estimate = summary_mean.loc[:, "mean"]
-corr_mean_fit = model_signal(map_estimate["amplitude"], map_estimate["centroid"], map_estimate["sigma"], map_estimate["baseline"], x)
+summary_mean = az.summary(trace_mean, var_names=["amplitude", "centroid", "sigma", "baseline", "sigma_noise","velocity", "snr"])
 
-dx = map_estimate["centroid"]*px #0.15e-3 # m
-dt = lagstep/fps # s
+axs = az.plot_posterior(trace_mean, var_names=["velocity", "snr"], figsize=(6,3));
+axs[0].set_title("")
+axs[1].set_title("")
+axs[0].set_xlabel("v ($\mu m/s$)")
+axs[1].set_xlabel("snr")
 
-v = dx/dt # mm/s
-print("sample velocity = {} mm/s".format(v*1e3))
-# -
+# Next, we use the resulting mean of the velocity distribution to perform the Gallilei transformation.
 
-# We use the resulting velocity to perform the Gallilei transformation.
+v = summary_mean["mean"]["velocity"]*1e-6
+print("Mean velocity of the sample is v = {} $mum /s$.".format(v*1e6))
 
-# +
 data_shifted = dataprep.shift_data(data, v, fps, px)
 
-time = 100
 fig,axs = plt.subplots(2,1,sharex=True, figsize=(10,5))
 axs[0].imshow(data.T, aspect="auto", origin="lower")
 axs[0].set_ylabel("frame (-)");
@@ -246,12 +229,50 @@ axs[1].set_xlabel("length (mm)")
 axs[1].set_ylabel("frame (-)");
 axs[1].set_title("shifted");
 
+# Observe that now all the samples are aligned. As a result we can average the frames to reduce the noise.
 
+# +
+data_mean = np.mean(data_shifted[:,startframe:endframe], axis=1).reshape(-1, 1)
+
+scaler = preprocessing.MinMaxScaler().fit(data_mean)
+data_mean = scaler.transform(data_mean).flatten()
 # -
 
-# Observe that now all the samples are aligned.
+# Again, we fit a Gaussian function to the intensity.
 
-# We now include a Bayesian hypothesis test or model comparison to derive at a conclusion for "sample present/not present". Therefore, we fit a noise-only model to the data too and compare the results using the Widely-applicable Information Criterion (step 4).
+x = np.linspace(0, len(data_mean), len(data_mean))
+model_wsignal = None
+with create_signalmodel(data_mean, x) as model:
+    trace_mean = pm.sample(10000, return_inferencedata=False, cores=4)
+    model_wsignal = model
+
+ppc = pm.sample_posterior_predictive(trace_mean, model=model_wsignal)
+idata = az.from_pymc3(
+        posterior_predictive=ppc,
+        model=models[0]
+    )
+
+# And display the fit together with the averaged intensity data and the .95 HDI.
+
+hdi = az.hdi(idata.posterior_predictive, hdi_prob=.95)
+fig,axs = plt.subplots(1,1,sharex=True, figsize=(10,5))
+axs.plot(x, data_mean, label="averaged", alpha=0.8, color="black")
+axs.plot(x, idata.posterior_predictive.mean(("chain", "draw"))["y"], color="red", label="fit")
+axs.fill_between(x, hdi["y"][:,0], hdi["y"][:,1], alpha=0.2, color="blue", label=".95 HDI")
+axs.legend();
+axs.set_xlabel("length (px)")
+axs.set_ylabel("intensity (AU)");
+
+# Furthermore, let's have a look at the snr:
+
+ax = az.plot_posterior(trace_mean, var_names=["snr"], figsize=(6,3));
+ax.set_title("")
+ax.set_xlabel("snr")
+
+
+# Note that our calculation of the snr includes the uncertainty of the data and the estimation. The HDI of the estimated signal-to-noise ratio is between 9 and 11, which is enough to conclude that a sample is present. 
+
+# We now include a Bayesian hypothesis test or model comparison to derive at a conclusion for "sample present/not present". Therefore, we fit a noise-only model to the data too and compare the results using the Widely-applicable Information Criterion.
 
 # +
 def create_model_noiseonly(data):
@@ -268,134 +289,24 @@ def create_model_noiseonly(data):
 
         return model
     
-with create_model_noiseonly(corr_mean) as model:
-    trace_noiseonly = pm.sample(10000, return_inferencedata=True)
+model_noise = None
+with create_model_noiseonly(data_mean) as model:
+    trace_noiseonly = pm.sample(10000, return_inferencedata=False, cores=4)
+    model_noise = model
 # -
 
-dfwaic = pm.compare({"sample":trace_mean, "noiseonly":trace_noiseonly}, ic="waic")
+dfwaic = pm.compare({"sample":trace_mean, "noiseonly":trace_noiseonly}, ic="loo")
 print(dfwaic)
 az.plot_compare(dfwaic, insample_dev=False);
+
+# Furthermore, we calculate the Bayes factor using bridge sampling:
+
+# +
+import bayesian
+ml_noiseonly = bayesian.Marginal_llk(trace_noiseonly, model_noise)
+ml_signal = bayesian.Marginal_llk(trace_mean, model_wsignal)
+
+print("Bayes factor: {}".format(np.exp(ml_signal["logml"]-ml_noiseonly["logml"])))
+# -
 
 # As a final result, we can be very certain that a sample is present.
-
-# Now let's try the same on a sample with a very low concentration.
-
-inname = "/home/cb51neqa/projects/itp/exp_data/ITP_AF647_5µA/AF_0.1ng_l/003.nd2"
-
-data_raw = dataprep.load_nd_data(inname, verbose=False)
-data_raw = dataprep.cuttochannel(data_raw, channel_lower, channel_upper)
-background = np.mean(data_raw[:,:,0:10],axis=2)
-data_raw = dataprep.substractbackground(data_raw, background)
-
-# + tags=[]
-height = data_raw.shape[0]
-length = data_raw.shape[1]
-nframes = data_raw.shape[2]
-print("height = {}, length = {}, nframes = {}".format(height, length, nframes))
-# -
-
-tmp = dataprep.averageoverheight(data_raw)
-
-scaler = preprocessing.StandardScaler().fit(tmp)
-data = scaler.transform(tmp)
-
-x = np.linspace(0, length*px*1e3, length)
-
-time = 100
-fig,axs = plt.subplots(2,1,sharex=True, figsize=(10,5))
-axs[0].plot(x, data[:,time])
-axs[0].set_title("frame = {}".format(time))
-axs[0].set_ylabel("intensity (AU)");
-axs[1].imshow(data.T, aspect="auto", origin="lower", extent=(0, length*px*1e3, 0, nframes))
-axs[1].set_xlabel("length (mm)")
-axs[1].set_ylabel("frame (-)");
-
-# We observe that the line representing the sample being transported through the channel is very weak. Now imaging that we are not able to see the line at all. Still if we would know the correct velocity of the sample we could again shift and average our frames. Otherwise, we again have to calculate the crosscorrelation and average the correlation functions.
-
-corr = dataprep.correlate_frames(data, 30)
-
-time = 200
-fig,axs = plt.subplots(2,1,sharex=True, figsize=(10,5))
-axs[0].plot(corr[:,time])
-axs[0].set_title("frame = {}".format(time))
-axs[0].set_ylabel("intensity (AU)");
-axs[1].imshow(corr.T, aspect="auto", origin="lower")
-axs[1].set_xlabel("lag ???")
-axs[1].set_ylabel("???");
-
-# +
-corr_mean = np.mean(corr[:,100:250], axis=1).reshape(-1, 1)
-x = np.linspace(-corr_mean.shape[0]/2, corr_mean.shape[0]/2, corr_mean.shape[0])
-
-# clean the correlation data
-# remove peak at zero lag
-corr_mean[int(corr_mean.shape[0]/2)] = 0
-#cut everything right of the middle (because we know that the velocity is positiv)
-corr_mean = corr_mean[0:int(corr_mean.shape[0]/2)]
-x = x[0:int(corr_mean.shape[0])]
-# -
-
-x *= -1
-
-corr_singleframe = corr[0:int(corr.shape[0]/2),time]
-
-# +
-scaler = preprocessing.MinMaxScaler().fit(corr_mean)
-corr_mean = scaler.transform(corr_mean).flatten()
-
-scaler = preprocessing.MinMaxScaler().fit(corr_singleframe.reshape(-1,1))
-corr_singleframe = scaler.transform(corr_singleframe.reshape(-1,1)).flatten()
-# -
-
-plt.plot(x, corr_singleframe, label="single frame")
-plt.plot(x, corr_mean, label="averaged")
-plt.legend();
-
-# +
-with create_signalmodel(corr_mean, x) as model:
-    trace_mean = pm.sample(20000, return_inferencedata=True, cores=4)
-
-with create_signalmodel(corr_singleframe, x) as model:
-    trace_singleframe = pm.sample(20000, return_inferencedata=True, cores=4)
-# -
-
-summary_mean = az.summary(trace_mean, var_names=["amplitude", "centroid", "sigma", "baseline", "sigma_noise"])
-summary_singleframe = az.summary(trace_singleframe, var_names=["amplitude", "centroid", "sigma", "baseline", "sigma_noise"])
-
-fig,axs = plt.subplots(1,1,sharex=True, figsize=(10,5))
-axs.plot(x, corr_mean, label="averaged", alpha=0.8)
-axs.plot(x, corr_singleframe, label="single", alpha=0.8)
-map_estimate = summary_singleframe.loc[:, "mean"]
-axs.plot(x, 
-    model_signal(map_estimate["amplitude"], map_estimate["centroid"], map_estimate["sigma"], map_estimate["baseline"], x), label="fit, single", alpha=0.5)
-map_estimate = summary_mean.loc[:, "mean"]
-axs.plot(x, 
-    model_signal(map_estimate["amplitude"], map_estimate["centroid"], map_estimate["sigma"], map_estimate["baseline"], x), label="fit, mean", alpha=0.5)
-axs.legend();
-
-# +
-map_estimate = summary_singleframe.loc[:, "mean"]
-snr_before = map_estimate["amplitude"]/map_estimate["sigma_noise"]
-map_estimate = summary_mean.loc[:, "mean"]
-snr_after = map_estimate["amplitude"]/map_estimate["sigma_noise"]
-
-print("snr_before = {}\nsnr_after = {}".format(snr_before, snr_after))
-print("improvement = snr_after/snr_before = {}".format(snr_after/snr_before))
-
-# +
-map_estimate = summary_mean.loc[:, "mean"]
-corr_mean_fit = model_signal(map_estimate["amplitude"], map_estimate["centroid"], map_estimate["sigma"], map_estimate["baseline"], x)
-
-dx = map_estimate["centroid"]*px #0.15e-3 # m
-dt = lagstep/fps # s
-
-v = dx/dt # mm/s
-print("sample velocity = {} mm/s".format(v*1e3))
-# -
-
-with create_model_noiseonly(corr_mean) as model:
-    trace_noiseonly = pm.sample(10000, return_inferencedata=True)
-
-dfwaic = pm.compare({"sample":trace_mean, "noiseonly":trace_noiseonly}, ic="waic")
-print(dfwaic)
-az.plot_compare(dfwaic, insample_dev=False);
