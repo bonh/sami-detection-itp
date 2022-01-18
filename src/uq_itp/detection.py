f276cf219f15ff23aa7e132a4a63d826b6fc1d5d
Henning Bonart <bonart@nmf.tu-darmstadt.de>
2022-01-18 10:51:48 +0100

    updated detction scipt

--- src/uq_itp/detection.py
+++ src/uq_itp/detection.py
@@ -15,6 +15,9 @@
 
 import arviz as az
 import numpy as np
+import re
+
+import bayesian
 
 # +
 concentrations = ["AF647_10ng_l", "AF647_1ng_l", "AF647_100pg_l", "AF647_10pg_l", "AF647_1pg_l", "AF647_0ng_l"]
@@ -37,30 +40,32 @@ def get_conc_name(concentration):
             
     if unit == "pg":
         conc = int(conc)/1000
-        
+
     return conc
 
+results = np.zeros((len(concentrations)*N, 5))
+results += np.nan
 
-results = np.zeros((len(concentrations), N, 3))
 for j in range(0, len(concentrations)):
     for i in range(0, N):
+        conc = get_conc_name(concentrations[j])
+        results[j*N+i,0:2] = np.array([conc, i+1])
         try:
             inname = "./{}/00{}/idata_cross.nc".format(concentrations[j], i+1)
             idata_cross = az.InferenceData.from_netcdf(inname) 
-            
+            v = int(bayesian.check_rope(idata_cross.posterior["velocity"], rope_velocity)>.95)
+            results[j*N+i,2] = np.array([v])
+
             inname = "./{}/00{}/idata.nc".format(concentrations[j], i+1)
             idata = az.InferenceData.from_netcdf(inname) 
-            
-            conc = get_conc_name(concentrations[j])
-            
-            v = bayesian.check_rope(idata.posterior["velocity"], rope_velocity)>.95 
-            w = bayesian.check_rope(idata3.posterior["sigma"], rope_sigma)>.95
-            snr = bayesian.check_refvalue(idata3.posterior["snr"], snr_ref)>.95
-            
-            results[conc, i] = np.array([v, w, snr])
+            w = int(bayesian.check_rope(idata.posterior["sigma"], rope_sigma)>.95)
+            snr = int(bayesian.check_refvalue(idata.posterior["snr"], ref_snr)>.95)
+            results[j*N+i,3:5] = np.array([w, snr])
+
         except FileNotFoundError as e:
             print(e)
-            results[conc, i] = np.array([np.nan, np.nan, np.nan])
             continue
 
+print(results)
 
+np.savetxt("detection.csv", results, header="c, i, v, w, snr", delimiter=",", comments='', fmt='%5g, %1.1g, %1.1g, %1.1g, %1.1g')
